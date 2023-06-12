4b92f93e1330d53cb1a5d6e484fbd39425187751
AngelaFlew <angelafliu2023@gmail.com>
2023-06-11 23:38:44 -0400

    Set up basic react-native app

---
+++ App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screens/HomeScreen';
+import NewEntryScreen from './screens/NewEntryScreen';
+import ProfileSettingsScreen from './screens/ProfileSettingsScreen';
+import './App.css'
+import MyTabBar from './MyTabBar';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="New Entry" component={NewEntryScreen} />
+        <Tab.Screen name="Profile Settings" component={ProfileSettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;

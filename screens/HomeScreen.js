a6933ee48515a43755b6a487c7fc0baecb25b831
AngelaFlew <angelafliu2023@gmail.com>
2023-06-11 23:39:01 -0400

    Add Home, NewEntry, and Settings tabs

---
+++ screens/HomeScreen.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  const [name, setName] = useState('');
+
+  const handleNameChange = (text) => {
+    setName(text);
+  };
+
+  const handleSubmit = () => {
+    console.log(`Hello, ${name}!`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Digia, your digital diary.</Text>
+      <Text style={styles.subtitle}>What is your name?</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={handleNameChange}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E5E7EF',
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: 'Verdana',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#2C3A47',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontFamily: 'Verdana',
+    marginBottom: 10,
+    color: '#2C3A47',
+    textAlign: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#2C3A47',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    fontFamily: 'Verdana',
+    color: '#2C3A47',
+    backgroundColor: '#FFFFFF',
+  },
+  button: {
+    backgroundColor: '#2C3A47',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Verdana',
+  },
+});
+
+export default HomeScreen;
---
+++ screens/NewEntryScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function NewEntryScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>New Entry Screen</Text>
+      </View>
+    );
+  }
+
+export default NewEntryScreen;
